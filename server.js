c5d3b9e891e14e112c12bd2177cbee525aae6e22
Max Wang <wangm13@rpi.edu>
2017-10-29 19:19:05 -0400

    First connection to mysql database

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: "psychpos.cma6st4fitis.us-east-2.rds.amazonaws.com",
+	port: "3306",
+	user: "team9",
+	password: "mitrfall17"
+});
+
+con.connect(function(err) {
+	if(err) throw err;
+	console.log("Connected!");
+});
 
 app.use(express.static('public_html'));
 
