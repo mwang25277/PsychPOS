3dce712b435537fb1f15ae9a209d01a7a836d66f
Max Wang <wangm13@rpi.edu>
2017-10-29 20:07:33 -0400

    Can insert/read from database

--- server.js
+++ server.js
@@ -9,13 +9,24 @@ var con = mysql.createConnection({
 	host: "psychpos.cma6st4fitis.us-east-2.rds.amazonaws.com",
 	port: "3306",
 	user: "team9",
-	password: "mitrfall17"
+	password: "mitrfall17",
+	database: "psychPOS"
 });
 
 con.connect(function(err) {
 	if(err) throw err;
 	console.log("Connected!");
-});
+	var sql = "INSERT INTO Employee (name, password, isAdmin) VALUES ('Max Wang', 'password', 0)";
+	con.query(sql, function (err, result) {
+		if(err) throw err;
+		console.log("1 record inserted");
+	});
+	con.query("SELECT * FROM Employee", function (err, result, fields) {
+    if (err) throw err;
+    	console.log(result);
+  });
+});	
+
 
 app.use(express.static('public_html'));
 
